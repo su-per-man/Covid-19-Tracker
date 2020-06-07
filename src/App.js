7c47f90aa6dbd58e337026f738002f947dfdfe5e
suman98kumar <suman98kumar@gmail.com>
2020-06-08 04:38:57 +0530

    Evergreen added - Sample Popover

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import * as Evergreen from 'evergreen-ui'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
+    <div>
+      <Evergreen.Popover position={Evergreen.Position.BOTTOM_LEFT}
+        content={
+          <Evergreen.Menu>
+            <Evergreen.Menu.Group>
+              <Evergreen.Menu.Item icon="people">Share...</Evergreen.Menu.Item>
+              <Evergreen.Menu.Item icon="circle-arrow-right">Move...</Evergreen.Menu.Item>
+              <Evergreen.Menu.Item icon="edit" secondaryText="⌘R">
+                Rename...
+        </Evergreen.Menu.Item>
+            </Evergreen.Menu.Group>
+            <Evergreen.Menu.Divider />
+            <Evergreen.Menu.Group>
+              <Evergreen.Menu.Item icon="trash" intent="danger">
+                Delete...
+        </Evergreen.Menu.Item>
+            </Evergreen.Menu.Group>
+          </Evergreen.Menu>
+        }
+      >
+        <Evergreen.Button marginRight={16}>With Icons</Evergreen.Button>
+      </Evergreen.Popover>
     </div>
   );
 }
